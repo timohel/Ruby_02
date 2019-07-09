9205378888a95f7239f07675ffddf3aef856e6b6
timohel <timohel@gmail.com>
2019-07-09 12:11:17 +0200

    ajout du fichier exo_05.rb

---
+++ exo_05.rb
@@ -0,0 +1,25 @@
+#affiche à l'écran la string "On va compter le nombre d'heures de travail à THP"
+puts "On va compter le nombre d'heures de travail à THP"
+# #{} intégre un calcul dans une string
+puts "Travail : #{10 * 5 * 11}"
+#affiche à l'écran la string "Et en secondes ?"
+puts "Et en secondes ?"
+#affiche le résultat d'un calcul
+puts 10 * 5 * 11 * 60 * 60
+# affiche à l'écran la string "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# affiche le résultat du calcul Boléen
+puts 3 + 2 < 5 - 7
+# affiche la sting "Ça fait combien 3 + 2 ?" puis le résultat du calcul 3 + 2
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#affiche à l'écran la string "Ok, c'est faux alors !"
+puts "Ok, c'est faux alors !"
+#affiche à l'écran la string "C'est drôle ça, faisons-en plus :"
+puts "C'est drôle ça, faisons-en plus :"
+# affiche la string "Est-ce que 5 est plus grand que puis le  résultat du calcul Boléen 5 supérieur à -2
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+# affiche la string Est-ce que 5 est supérieur ou égal à -2 ? puis le résultat du calcul Boléen 5 supérieur ou égal à -2
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+# affiche la string Est-ce que 5 est inférieur ou égal à -2 ? puis le résultat du calcul Boléen 5 inférieur ou égal à -2
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
